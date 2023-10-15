5ac2adca3dfb768ca2d1fc62911b1115ef4c4c5c
YURI HONG <dbfl720@gmail.com>
2023-10-15 14:00:10 +0900

    cors 수정

--- src/main/java/com/smartChart/BackEnd/WebConfig.java
+++ src/main/java/com/smartChart/BackEnd/WebConfig.java
@@ -12,8 +12,18 @@ public class WebConfig implements WebMvcConfigurer {
      *
      * ※크로스 도메인 이슈: 브라우저에서 다른 도메인으로 URL 요청을 하는 경우 나타나는 보안문제
      */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+//    }
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000") // 허용할 출처
+                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
