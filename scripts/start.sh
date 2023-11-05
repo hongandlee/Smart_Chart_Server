e493d8c4405023a625fc2016240e594a169d9ff4
yuri <yuri@lineus.kr>
2023-11-05 19:27:03 +0900

    start.sh 경로 다시 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,13 +7,16 @@ APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
 START_LOG="$ROOT_PATH/start.log"
 
+# JAR 파일의 실제 경로로 수정
+JAR_SOURCE="/home/ubuntu/spring-github-action/build/libs/application.jar"
+
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/application.jar $JAR
+cp "$JAR_SOURCE" "$JAR"
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar "$JAR" > "$APP_LOG" 2> "$ERROR_LOG" &
 
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+SERVICE_PID=$(pgrep -f "$JAR")
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> "$START_LOG"
