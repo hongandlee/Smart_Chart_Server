2064ebff2d9d3ff6397a0bbd6c9afbb2be66a241
yuri <yuri@lineus.kr>
2023-11-06 00:21:28 +0900

    다시 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/spring-github-action/scripts"
+ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/application.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
-
-# JAR 파일의 실제 경로로 수정
-JAR_SOURCE="/home/ubuntu/spring-github-action/build/libs/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
@@ -14,10 +10,10 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp "$JAR_SOURCE" "$JAR"
+cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar "$JAR" > "$APP_LOG" 2> "$ERROR_LOG" &
+nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(pgrep -f "$JAR")
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> "$START_LOG"
+SERVICE_PID=$(pgrep -f $JAR)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,16 +3,12 @@
 ROOT_PATH="/home/ubuntu/spring-github-action/scripts"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
-
-# JAR 파일의 실제 경로로 수정
-JAR_SOURCE="/home/ubuntu/spring-github-action/build/libs/application.jar"
-
-SERVICE_PID=$(pgrep -f "$JAR_SOURCE") # 실행중인 Spring 서버의 PID
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 Not Found" >> "$STOP_LOG"
+  echo "서비스 NouFound" >> $STOP_LOG
 else
-  echo "서비스 종료 " >> "$STOP_LOG"
+  echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
