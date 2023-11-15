ce2debb66db14b933713e57474729499438b0f31
yuri <yuri@lineus.kr>
2023-11-15 17:20:55 +0900

    cors 수정

--- src/main/java/com/smartChart/BackEnd/WebConfig.java
+++ src/main/java/com/smartChart/BackEnd/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000") // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000","http://13.125.227.145:8080") // 허용할 출처
                 .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
