2fbca37e314fa914570a73a23fb70580748862f9
yuri <yuri@lineus.kr>
2023-11-28 20:51:45 +0900

    origin push2

--- src/main/java/com/smartChart/BackEnd/WebConfig.java
+++ src/main/java/com/smartChart/BackEnd/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000","http://13.125.227.145:8080","https://smartchart-5014f0kzt-jeongmin.vercel.app/") // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000","http://13.125.227.145:8080","https://smartchart.vercel.app/") // 허용할 출처
                 .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(10800); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
