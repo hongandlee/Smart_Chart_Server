f36726fe6391d41753c5d7c27defa99407f5fefc
yuri <yuri@lineus.kr>
2023-11-28 22:16:45 +0900

    smart 서버 관련 주소 수정

--- src/main/java/com/smartChart/config/SecurityConfiguration.java
+++ src/main/java/com/smartChart/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration  {
                 .antMatchers("/codes/**").permitAll()
                 .antMatchers( "/doctor/**")
                 .permitAll()
-                .antMatchers("/","/css/**","/index.html","/main.js","/images/**","/js/**","/image/**", "/oauth2/**","/login/oauth2/**","/auth/**","/auth/kakao/callback","/websocket/**", "/static/**","/api/**","/naver/**","/kakaoPay/**", "/vertifyIamport/**","/chatting-view/**","/adminAppointment/**", "/adminWaitingList/**","/hospitalPage/**","/accounting/**","/https://smartchart.vercel.app/**").permitAll()
+                .antMatchers("/","/css/**","/index.html","/main.js","/images/**","/js/**","/image/**", "/oauth2/**","/login/oauth2/**","/auth/**","/auth/kakao/callback","/websocket/**", "/static/**","/api/**","/naver/**","/kakaoPay/**", "/vertifyIamport/**","/chatting-view/**","/adminAppointment/**", "/adminWaitingList/**","/hospitalPage/**","/accounting/**","/https://smartchart.vercel.app/**","https://smartchart.vercel.app/auth/kakao/callback").permitAll()
                 .antMatchers("/login", "/join").permitAll() // 로그인, 회원가입 접근 가능
                 .antMatchers("/ws/**","/ws/chat").permitAll()
 
