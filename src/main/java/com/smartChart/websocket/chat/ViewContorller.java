0c7dcaedc9d6a89237134f0b296ba9b0878b5569
yuri <yuri@lineus.kr>
2023-11-30 18:06:08 +0900

    kakao redirect 주소 변경

--- src/main/java/com/smartChart/websocket/chat/ViewContorller.java
+++ src/main/java/com/smartChart/websocket/chat/ViewContorller.java
@@ -1,19 +1,14 @@
 package com.smartChart.websocket.chat;
 
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-@RequiredArgsConstructor
-public class ViewContorller {
-
-    // 채팅 리스트 화면
-    @GetMapping("/chatting-view")
-    public String chatting(Model model) {
-
-        return "index";
-    }
-}
+//@Controller
+//@RequiredArgsConstructor
+//public class ViewContorller {
+//
+//    // 채팅 리스트 화면
+//    @GetMapping("/chatting-view")
+//    public String chatting(Model model) {
+//
+//        return "index";
+//    }
+//}
