dfa5d9a8fd73339cbd3eb2d5e7c49d2943707906
yuri <yuri@lineus.kr>
2023-12-18 16:40:07 +0900

    kakao

--- src/main/java/com/smartChart/BackEnd/WebConfig.java
+++ src/main/java/com/smartChart/BackEnd/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080/", "http://localhost:3000","http://13.125.227.145:8080/**","http://13.125.227.145/**","https://smartchart.vercel.app/","ws://localhost:8080/ws/chat/","https://smartchart.vercel.app/auth/kakao/callback/","ws://localhost:3000/ws/chat/","https://smartchart.p-e.kr","wss://smartchart.p-e.kr/ws/chat/","http://13.125.227.145/**","https://13.125.227.145/**"," https://www.smartchart.store/**","https://smartchart.shop/**") // 허용할 출처
+                .allowedOrigins("http://localhost:8080/", "http://localhost:3000","http://13.125.227.145:8080/**","http://13.125.227.145/**","https://smartchart.vercel.app/**","ws://localhost:8080/ws/chat/","https://smartchart.vercel.app/auth/kakao/callback/","ws://localhost:3000/ws/chat/","https://smartchart.p-e.kr","wss://smartchart.p-e.kr/ws/chat/","http://13.125.227.145/**","https://13.125.227.145/**"," https://www.smartchart.store/**","https://smartchart.shop/**") // 허용할 출처
                 .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(10800); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
