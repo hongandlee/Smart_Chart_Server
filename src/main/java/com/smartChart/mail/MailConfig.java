1825fc81c036797f77917b014944d01e6aad6716
yuri <yuri@lineus.kr>
2023-12-19 15:42:25 +0900

    original

---
+++ src/main/java/com/smartChart/mail/MailConfig.java
@@ -0,0 +1,38 @@
+package com.smartChart.mail;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setUsername("dbfl720@gmail.com");
+        javaMailSender.setPassword("lavwqdbcspqkjaxm");
+
+        javaMailSender.setPort(587);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.gmail.com");
+        properties.setProperty("mail.smtp.ssl.enable","false");
+        properties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+        return properties;
+    }
+}
